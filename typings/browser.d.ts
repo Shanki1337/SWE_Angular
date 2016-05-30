339fec2b7581767bd1aa63af708a099a89413535
Shankara Isha Berger <besh1011(at)hs-karlsruhe.de>
2016-05-30 13:46:18 +0200

    [Project - F] Initial Commit

---
+++ typings/browser.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="browser/ambient/jwt-decode/index.d.ts" />
+/// <reference path="browser/ambient/whatwg-fetch/index.d.ts" />
+/// <reference path="browser/definitions/lodash/index.d.ts" />
+/// <reference path="browser/definitions/moment/index.d.ts" />
---
+++ typings/main.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="main/ambient/jwt-decode/index.d.ts" />
+/// <reference path="main/ambient/whatwg-fetch/index.d.ts" />
+/// <reference path="main/definitions/lodash/index.d.ts" />
+/// <reference path="main/definitions/moment/index.d.ts" />
