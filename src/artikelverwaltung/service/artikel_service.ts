b77c75cca1839da9495c21121691c727ced91af8
Shankara Isha Berger <besh1011(at)hs-karlsruhe.de>
2016-06-07 08:14:45 +0200

    [Project - F] Anpassungen Artikel-Service
    ToDo: find & save implementieren bzw. anpassen

--- src/artikelverwaltung/service/artikel_service.ts
+++ src/artikelverwaltung/service/artikel_service.ts
@@ -21,28 +21,16 @@ import {Http, Response, Headers, RequestOptionsArgs, URLSearchParams} from 'angu
 
 // Moment exportiert den Namespace moment und die gleichnamige Function:
 // http://stackoverflow.com/questions/35254524/using-moment-js-in-angular-2-typescript-application#answer-35255412
-<<<<<<< HEAD
-/* import {Moment} from 'moment';
-import * as moment_ from 'moment';
-const moment: (date: Date) => Moment = (<any>moment_)['default'];
-*/
-=======
 import {Moment} from 'moment';
 import * as moment_ from 'moment';
 const moment: (date: Date) => Moment = (<any>moment_)['default'];
 
->>>>>>> origin/master
 import {IChart, ChartDataSet, LinearChartData, CircularChartData} from 'chart.js/Chart';
 
 import Artikel from '../model/artikel';
 import {IArtikelServer, IArtikelForm} from '../model/artikel';
-<<<<<<< HEAD
-import AbstractArtikelService from './abstract_artikel_service';
-import {ChartService, BASE_URI, PATH_ARTIKEL, isBlank, isPresent, isEmpty, log} from '../../shared/shared';
-=======
 // import AbstractArtikelService from './abstract_artikel_service';
 import {ChartService, BASE_URI, PATH_ARTIKEL, PATH_KATALOG , isBlank, isPresent, isEmpty, log} from '../../shared/shared';
->>>>>>> origin/master
 import {getAuthorization} from '../../iam/iam';
 /* tslint:enable:max-line-length */
 
@@ -60,15 +48,6 @@ import {getAuthorization} from '../../iam/iam';
 // Die Anwendungslogik wird vom Controller an Service-Klassen delegiert.
 
 /**
-<<<<<<< HEAD
- * Die Service-Klasse zu Artikel.
- */
-export default class ArtikelService extends AbstractArtikelService {
-    private _baseUriArtikel: string;
-    private _artikelEmitter: EventEmitter<Array<Artikel>> =
-        new EventEmitter<Array<Artikel>>();
-    private _artikelSEmitter: EventEmitter<Artikel> =
-=======
  * Die Service-Klasse zu B&uuml;cher.
  */
 export default class ArtikelService {
@@ -77,7 +56,6 @@ export default class ArtikelService {
     private _artikelzEmitter: EventEmitter<Array<Artikel>> =
         new EventEmitter<Array<Artikel>>();
     private _artikelEmitter: EventEmitter<Artikel> =
->>>>>>> origin/master
         new EventEmitter<Artikel>();
     private _errorEmitter: EventEmitter<string|number> =
         new EventEmitter<string|number>();
@@ -91,34 +69,6 @@ export default class ArtikelService {
     constructor(
         @Inject(ChartService) private _chartService: ChartService,
         @Inject(Http) private _http: Http) {
-<<<<<<< HEAD
-        super();
-        this._baseUriArtikel = `${BASE_URI}${PATH_ARTIKEL}`;
-        console.log(
-            `ArtikelService.constructor(): baseUriArtikel=${this._baseUriArtikel}`);
-    }
-
-    /**
-     * Ein Artikel-Objekt puffern.
-     * @param artikel Das Artikel-Objekt, das gepuffert wird.
-     * @return void
-     */
-    set artikelS(artikelS: Artikel) {
-        console.log('ArtikelService.set artikelS()', artikelS);
-        this._artikel = artikelS;
-    }
-
-    @log
-    observeArtikels(
-        observerFn: (artikel: Array<Artikel>) => void, thisArg: any): void {
-        this._artikelEmitter.forEach(observerFn, thisArg);
-    }
-
-    @log
-    observeArtikel(observerFn: (artikelS: Artikel) => void, thisArg: any):
-        void {
-        this._artikelSEmitter.forEach(observerFn, thisArg);
-=======
         this._baseUriArtikel = `${BASE_URI}${PATH_ARTIKEL}`;
         this._baseUriKatalog = `${BASE_URI}${PATH_KATALOG}`;
         console.log(
@@ -147,7 +97,6 @@ export default class ArtikelService {
     @log
     observeArtikel(observerFn: (artikel: Artikel) => void, thisArg: any): void {
         this._artikelEmitter.forEach(observerFn, thisArg);
->>>>>>> origin/master
     }
 
     @log
@@ -155,63 +104,6 @@ export default class ArtikelService {
         this._errorEmitter.forEach(observerFn, thisArg);
     }
 
-<<<<<<< HEAD
-
-    @log
-    find(suchkriterien: IArtikelForm): void {
-        console.log('ALLES IST GUT! #################');
-        const searchParams: URLSearchParams =
-            this._suchkriterienToSearchParams(suchkriterien);
-        const uri: string = this._baseUriArtikel;
-        console.log(`ArtikelService.find(): uri=${uri}`);
-
-        const nextFn: ((response: Response) => void) = (response: Response) => {
-            console.log(`ArtikelService.find(): nextFn()`);
-            let artikel: Array<Artikel> =
-                this._responseToArrayArtikel(response);
-            this._artikelEmitter.emit(artikel);
-        };
-        const errorFn: (err: Response) => void = (err: Response) => {
-            const status: number = err.status;
-            console.log(`ArtikelService.find(): errorFn(): ${status}`);
-            if (status === 400) {
-                const body: string = err.text();
-                if (isBlank(body)) {
-                    this._errorEmitter.emit(status);
-                } else {
-                    // z.B. [PARAMETER][findByTitel.titel][Bei einem ...][x]
-                    let errorMsg: string = body.split('[')[3];
-                    errorMsg = errorMsg.substr(0, errorMsg.length - 2);
-                    this._errorEmitter.emit(errorMsg);
-                }
-            } else {
-                this._errorEmitter.emit(status);
-            }
-        };
-
-        this._http.get(uri, {search: searchParams}).subscribe(nextFn, errorFn);
-    }
-    /**
-     * Artikel suchen
-     * @param suchkriterien Die Suchkriterien
-     */
-    @log
-    find2(suchkriterien: IArtikelForm): void {
-        const searchParams: URLSearchParams =
-            this._suchkriterienToSearchParams(suchkriterien);
-        const uri: string = this._baseUriArtikel;
-        console.log(`ArtikelService.find(): uri=${uri}`);
-
-        const nextFn: ((response: Response) => void) = (response: Response) => {
-            console.log('ArtikelService.find(): nextFn()');
-            let artikel: Array<Artikel> =
-                this._responseToArrayArtikel(response);
-            this._artikelEmitter.emit(artikel);
-        };
-        const errorFn: (err: Response) => void = (err: Response) => {
-            const status: number = err.status;
-            console.log(`ArtikelService.find(): errorFn(): ${status}`);
-=======
     /**
      * Buecher suchen
      * @param suchkriterien Die Suchkriterien
@@ -231,7 +123,6 @@ export default class ArtikelService {
         const errorFn: (err: Response) => void = (err: Response) => {
             const status: number = err.status;
             console.log(`BuecherService.find(): errorFn(): ${status}`);
->>>>>>> origin/master
             if (status === 400) {
                 const body: string = err.text();
                 if (isBlank(body)) {
@@ -251,16 +142,6 @@ export default class ArtikelService {
     }
 
     /**
-<<<<<<< HEAD
-     * Ein Artikel anhand der ID suchen
-     * @param id Die ID des gesuchten Artikels
-     */
-    @log
-    findById(id: string): void {
-        // Gibt es ein gepuffertes Artikel mit der gesuchten ID?
-        if (isPresent(this._artikel) && this._artikel._id === id) {
-            this._artikelSEmitter.emit(this._artikel);
-=======
      * Ein Buch anhand der ID suchen
      * @param id Die ID des gesuchten Buchs
      */
@@ -269,24 +150,16 @@ export default class ArtikelService {
         // Gibt es ein gepuffertes Buch mit der gesuchten ID?
         if (isPresent(this._artikel) && this._artikel._id === id) {
             this._artikelEmitter.emit(this._artikel);
->>>>>>> origin/master
             return;
         }
         if (isBlank(id)) {
             return;
         }
 
-<<<<<<< HEAD
-        const uri: string = `${this._baseUriArtikel}/${id}`;
-        const nextFn: ((response: Response) => void) = (response: Response) => {
-            this._artikel = this._responseToArtikel(response);
-            this._artikelSEmitter.emit(this._artikel);
-=======
         const uri: string = `${this._baseUriKatalog}/${id}`;
         const nextFn: ((response: Response) => void) = (response: Response) => {
             this._artikel = this._responseToArtikel(response);
             this._artikelEmitter.emit(this._artikel);
->>>>>>> origin/master
         };
         const errorFn: (err: Response) => void = (err: Response) => {
             const status: number = err.status;
@@ -297,11 +170,6 @@ export default class ArtikelService {
         this._http.get(uri).subscribe(nextFn, errorFn);
     }
 
-<<<<<<< HEAD
-    /**
-     * Ein neues Artikel anlegen
-     * @param neuesArtikel Das JSON-Objekt mit dem neuen Artikel
-=======
     @log
     remove(
         artikel: Artikel, successFn: () => void,
@@ -319,25 +187,17 @@ export default class ArtikelService {
     /**
      * Ein neues Buch anlegen
      * @param neuesBuch Das JSON-Objekt mit dem neuen Buch
->>>>>>> origin/master
      * @param successFn Die Callback-Function fuer den Erfolgsfall
      * @param errorFn Die Callback-Function fuer den Fehlerfall
      */
     @log
     save(
-<<<<<<< HEAD
-        neuerArtikel: Artikel, successFn: (location: string) => void,
-        errorFn: (status: number, text: string) => void): void {
-        const uri: string = this._baseUriArtikel;
-        const body: string = JSON.stringify(neuerArtikel.toJSON());
-=======
         neuesBuch: Buch, successFn: (location: string) => void,
         errorFn: (status: number, text: string) => void): void {
         neuesBuch.datum = moment(new Date());
 
         const uri: string = this._baseUriBuecher;
         const body: string = JSON.stringify(neuesBuch.toJSON());
->>>>>>> origin/master
         console.log('body=', body);
 
         const headers: Headers =
@@ -365,77 +225,6 @@ export default class ArtikelService {
         this._http.post(uri, body, options).subscribe(nextFn, errorFnPost);
     }
 
-<<<<<<< HEAD
-    /**
-     * Ein vorhandenes Artikel aktualisieren
-     * @param artikelS Das JSON-Objekt mit den aktualisierten Artikeldaten
-     * @param successFn Die Callback-Function fuer den Erfolgsfall
-     * @param errorFn Die Callback-Function fuer den Fehlerfall
-     */
-    @log
-    update(
-        artikelS: Artikel, successFn: () => void,
-        errorFn: (status: number, text: string) => void): void {
-        const uri: string = `${this._baseUriArtikel}`;
-        const body: string = JSON.stringify(artikelS.toJSON());
-        console.log('body=', body);
-
-        const headers: Headers =
-            new Headers({'Content-Type': 'application/json'});
-        headers.append('Authorization', getAuthorization());
-        // RequestOptionsArgs in
-        // node_modules\angular2\ts\src\http\interfaces.ts
-        const options: RequestOptionsArgs = {headers: headers};
-        console.log('options=', options);
-
-        const nextFn: ((response: Response) => void) =
-            (response: Response) => { successFn(); };
-        const errorFnPut: ((errResponse: Response) => void) =
-            (errResponse: Response) => {
-                if (isPresent(errorFn)) {
-                    errorFn(errResponse.status, errResponse.text());
-                }
-            };
-
-        this._http.put(uri, body, options).subscribe(nextFn, errorFnPut);
-    }
-
-    /**
-     * Ein Artikel l&ouml;schen
-     * @param artikelS Das JSON-Objekt mit dem zu loeschenden Artikel
-     * @param successFn Die Callback-Function fuer den Erfolgsfall
-     * @param errorFn Die Callback-Function fuer den Fehlerfall
-     */
-    @log
-    remove(
-        artikelS: Artikel, successFn: () => void,
-        errorFn: (status: number) => void): void {
-        const uri: string = `${this._baseUriArtikel}/${artikelS._id}`;
-        const headers: Headers =
-            new Headers({'Authorization': getAuthorization()});
-        // RequestOptionsArgs in
-        // node_modules\angular2\ts\src\http\interfaces.ts
-        const options: RequestOptionsArgs = {headers: headers};
-        console.log('options=', options);
-
-        const nextFn: ((response: Response) => void) = (response: Response) => {
-            if (isPresent(successFn)) {
-                successFn();
-            }
-        };
-        const errorFnDelete: ((errResponse: Response) => void) =
-            (errResponse: Response) => {
-                if (isPresent(errorFn)) {
-                    errorFn(errResponse.status);
-                }
-            };
-
-
-        this._http.delete(uri, options).subscribe(nextFn, errorFnDelete);
-    }
-
-=======
->>>>>>> origin/master
     // http://www.sitepoint.com/15-best-javascript-charting-libraries
     // http://thenextweb.com/dd/2015/06/12/20-best-javascript-chart-libraries
     // http://mikemcdearmon.com/portfolio/techposts/charting-libraries-using-d3
@@ -529,20 +318,12 @@ export default class ArtikelService {
     }
 
     toString(): String {
-<<<<<<< HEAD
-        return `ArtikelService: {artikelS: ${JSON.stringify(this._artikel, null, 2)}}`;
-    }
-
-    /**
-     * Ein Response-Objekt in ein Array von Artikel-Objekten konvertieren.
-=======
         return `ArtikelService:
             {artikel: ${JSON.stringify(this._artikel, null, 2)}}`;
     }
 
     /**
      * Ein Response-Objekt in ein Array von Buch-Objekten konvertieren.
->>>>>>> origin/master
      * @param response Response-Objekt eines GET-Requests.
      */
     @log
@@ -550,16 +331,8 @@ export default class ArtikelService {
         URLSearchParams {
         const searchParams: URLSearchParams = new URLSearchParams();
 
-<<<<<<< HEAD
-        if (!isEmpty(suchkriterien.bezeichnung)) {
-            searchParams.set('bezeichnung', suchkriterien.bezeichnung);
-        }
-        if (isPresent(suchkriterien.rating)) {
-            searchParams.set('rating', suchkriterien.rating.toString());
-=======
         if (!isEmpty(suchkriterien._id)) {
-            searchParams.set('id', suchkriterien._id);
->>>>>>> origin/master
+            searchParams.set('isd', suchkriterien._id);
         }
         if (!isEmpty(suchkriterien.bezeichnung)) {
             searchParams.set('bezeichnung', suchkriterien.bezeichnung);
@@ -568,11 +341,7 @@ export default class ArtikelService {
     }
 
     /**
-<<<<<<< HEAD
-     * Ein Response-Objekt in ein Array von Artikel-Objekten konvertieren.
-=======
      * Ein Response-Objekt in ein Array von Buch-Objekten konvertieren.
->>>>>>> origin/master
      * @param response Response-Objekt eines GET-Requests.
      */
     @log
@@ -585,11 +354,7 @@ export default class ArtikelService {
     }
 
     /**
-<<<<<<< HEAD
-     * Ein Response-Objekt in ein Artikel-Objekt konvertieren.
-=======
      * Ein Response-Objekt in ein Buch-Objekt konvertieren.
->>>>>>> origin/master
      * @param response Response-Objekt eines GET-Requests.
      */
     @log
@@ -602,31 +367,18 @@ export default class ArtikelService {
      * Ein Balkendiagramm erzeugen und bei einem Tag <code>canvas</code>
      * einf&uuml;gen.
      * @param chartElement Das HTML-Element zum Tag <code>canvas</code>
-<<<<<<< HEAD
-     * @param artikel Die zu ber&uecksichtigenden B&uuml;cher
-     */
-    @log
-    private _createBarChart(
-        chartElement: HTMLCanvasElement, artikel: Array<Artikel>): void {
-        const labels: Array<string> =
-            artikel.map((artikelS: Artikel) => artikelS._id);
-=======
      * @param buecher Die zu ber&uecksichtigenden B&uuml;cher
      */
     @log
     private _createBarChart(
         chartElement: HTMLCanvasElement, artikelz: Array<Artikel>): void {
-        const labels: Array<string> = artikelz.map((artikel: Artikel) => artikel._id);
->>>>>>> origin/master
+        const labels: Array<string> =
+            artikelz.map((artikel: Artikel) => artikel._id);
         const datasets: Array<ChartDataSet> = [{
             label: 'Bewertungen',
             fillColor: 'rgba(220,220,220,0.2)',
             strokeColor: 'rgba(220,220,220,1)',
-<<<<<<< HEAD
-            data: artikel.map((artikelS: Artikel) => artikelS.rating)
-=======
             data: artikelz.map((artikel: Artikel) => artikel.rating)
->>>>>>> origin/master
         }];
         const data: LinearChartData = {labels: labels, datasets: datasets};
         console.log('ArtikelService._createBarChart(): labels: ', labels);
@@ -642,31 +394,18 @@ export default class ArtikelService {
      * Ein Liniendiagramm erzeugen und bei einem Tag <code>canvas</code>
      * einf&uuml;gen.
      * @param chartElement Das HTML-Element zum Tag <code>canvas</code>
-<<<<<<< HEAD
-     * @param artikel Die zu ber&uecksichtigenden B&uuml;cher
-     */
-    @log
-    private _createLineChart(
-        chartElement: HTMLCanvasElement, artikel: Array<Artikel>): void {
-        const labels: Array<string> =
-            artikel.map((artikelS: Artikel) => artikelS._id);
-=======
      * @param buecher Die zu ber&uecksichtigenden B&uuml;cher
      */
     @log
     private _createLineChart(
         chartElement: HTMLCanvasElement, artikelz: Array<Artikel>): void {
-        const labels: Array<string> = artikelz.map((artikel: Artikel) => artikel._id);
->>>>>>> origin/master
+        const labels: Array<string> =
+            artikelz.map((artikel: Artikel) => artikel._id);
         const datasets: Array<ChartDataSet> = [{
             label: 'Bewertungen',
             fillColor: 'rgba(220,220,220,0.2)',
             strokeColor: 'rgba(220,220,220,1)',
-<<<<<<< HEAD
-            data: artikel.map((artikelS: Artikel) => artikelS.rating)
-=======
             data: artikelz.map((artikel: Artikel) => artikel.rating)
->>>>>>> origin/master
         }];
         const data: LinearChartData = {labels: labels, datasets: datasets};
 
@@ -686,21 +425,6 @@ export default class ArtikelService {
      * Ein Tortendiagramm erzeugen und bei einem Tag <code>canvas</code>
      * einf&uuml;gen.
      * @param chartElement Das HTML-Element zum Tag <code>canvas</code>
-<<<<<<< HEAD
-     * @param artikel Die zu ber&uecksichtigenden B&uuml;cher
-     */
-    @log
-    private _createPieChart(
-        chartElement: HTMLCanvasElement, artikel: Array<Artikel>): void {
-        const pieData: Array<CircularChartData> =
-            new Array<CircularChartData>(artikel.length);
-        artikel.forEach((artikelS: Artikel, i: number) => {
-            const data: CircularChartData = {
-                value: artikelS.rating,
-                color: this._chartService.getColorPie(i),
-                highlight: this._chartService.getHighlightPie(i),
-                label: `${artikelS._id}`
-=======
      * @param buecher Die zu ber&uecksichtigenden B&uuml;cher
      */
     @log
@@ -714,7 +438,6 @@ export default class ArtikelService {
                 color: this._chartService.getColorPie(i),
                 highlight: this._chartService.getHighlightPie(i),
                 label: `${artikel._id}`
->>>>>>> origin/master
             };
             pieData[i] = data;
         });
